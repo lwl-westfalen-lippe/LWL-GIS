71d6b98e928ff3d9ec51ae5b70521441397e5949
mpfeil <matthias.pfeil@wwu.de>
2014-12-12 14:25:59 +0100

    start writing own modules

---
+++ js/modules/Classifications.js
@@ -0,0 +1,27 @@
+define(['data/DataHandling'], function(DataHandling) {
+
+  var classes = [];
+
+  return {
+    equalInterval: function (data, numberOfClasses) {
+      return classes;
+    },
+
+    quantile: function (data, numberOfClasses) {
+      return classes;
+    },
+
+    jenks: function (data, numberOfClasses) {
+      return classes;
+    },
+
+    standardDeviation: function (data, numberOfClasses) {
+      return classes;
+    },
+
+    pretty: function (data, numberOfClasses) {
+      return classes;
+    }
+  }
+
+});
