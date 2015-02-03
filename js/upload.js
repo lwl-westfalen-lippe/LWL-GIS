7ca5b69f9ab47e2ae0825cf0beb25ad85b62ebe5
mpfeil <matthias.pfeil@wwu.de>
2015-02-03 10:44:08 +0100

    removeable columns

--- js/upload.js
+++ js/upload.js
@@ -6,11 +6,25 @@ function hide () {
 }
 
 function addColumn () {
-  var colHeader = '<th>Jahr <a class="enabled" onclick="removeColumn();"><img src="images/close20.png"></a></th>';
-  var colData = '<td><input type="number" name="" value="" placeholder=""></td>';
   thead = $('#upload thead tr th');
+  var colID = thead.length-1;
+  colID = "year"+colID;
+  console.log(colID);
+  var colHeader = '<th class="'+colID+'">Jahr <input type="number" name="" value="" placeholder=""><a class="enabled" onclick="removeColumn(this);"><img src="images/close20.png"></a></th>';
+  var colData = '<td class="'+colID+'"><input type="number" name="" value="" placeholder=""></td>';
   $('#upload thead tr > th:nth-child('+(thead.length-1)+')').after(colHeader);
   $('#upload tbody tr > td:last-child').after(colData);
+}
+
+function removeColumn (elem) {
+  nextSiblings = $("."+elem.parentNode.className).nextUntil('#add');
+  $("."+elem.parentNode.className).remove();
+  $.each(nextSiblings, function (index,value) {
+    className = value.className;
+    lastDigit = className.slice(-1);
+    newID = "year"+(parseInt(lastDigit)-1);
+    value.className = newID;
+  });
 }
 
 
